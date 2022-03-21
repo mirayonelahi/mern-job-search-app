b0f1409c83996ad08e427a9e4e3965f8f46ea515
Ayon <mirelahi33@gmail.com>
2022-03-20 20:18:27 -0400

    add styled components and all styles in wrapper folder

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,9 +1,13 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
+      <nav>
+        <img src={logo} alt="job hunt" className="logo" />
+      </nav>
       <div className="container page">
         {/* info */}
         <div className="info">
@@ -20,7 +24,8 @@ const Landing = () => {
         </div>
         <img src={main} alt="job hunt" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
 export default Landing;
