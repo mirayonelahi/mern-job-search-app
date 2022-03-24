8bd16aba21808bcdbefe02e6f81c0d62f7b298f6
Ayon <mirelahi33@gmail.com>
2022-03-24 18:10:08 -0400

    basic generic form row component added

---
+++ client/src/components/FormRow.js
@@ -0,0 +1,18 @@
+const FormRow = ({ name, type, value, labelText, handleChange }) => {
+  return (
+    <div className="form-row">
+      <label htmlFor={name} className="form-label">
+        {labelText || name}
+      </label>
+
+      <input
+        type={type}
+        value={value}
+        name={name}
+        onChange={handleChange}
+        className="form-input"
+      />
+    </div>
+  );
+};
+export default FormRow;
--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,4 +1,54 @@
+import { useState, useEffect } from "react";
+import { Logo, FormRow } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
 const Register = () => {
-  return <div>Register</div>;
+  const initialState = {
+    name: "",
+    email: "",
+    password: "",
+    isMember: true,
+  };
+
+  const [values, setValues] = useState(initialState);
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* Name input */}
+        <FormRow
+          name="name"
+          type="text"
+          value={values.name}
+          handleChange={handleChange}
+        />
+
+        {/* Email input */}
+        <FormRow
+          name="email"
+          type="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+
+        {/* passwprd input */}
+        <FormRow
+          name="password"
+          type="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
+        <button className="btn btn-block">Submit</button>
+      </form>
+    </Wrapper>
+  );
 };
 export default Register;
